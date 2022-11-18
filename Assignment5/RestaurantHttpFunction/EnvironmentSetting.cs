5cecc60e569cbeb4c159c06f69982aa1ed30bd65
Gina Sun <ginasun28@gmail.com>
2022-11-17 17:12:37 -0800

    Assignment5 revised

---
+++ Assignment5/RestaurantHttpFunction/EnvironmentSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public static class EnvironmentSetting
+    {
+        public static string DBEnvironment = "RestaurantAzureDatabase";
+    
+    }
+}
---
+++ Assignment5/RestaurantHttpFunction/ResturantData/IConnectConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5.ResturantDataSQL
+{
+    public interface IConnectConfiguration
+    {
+        string GetConnectionString(string connectionString);
+    }
+}
